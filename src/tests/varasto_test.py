44b75beb06989da6c74c3f32a8c4d6af6ad0be00
Jpentik <juha.ao.pentikainen@gmail.com>
2024-11-04 21:18:51 +0200

    Actions-testi

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -70,4 +70,4 @@ class TestVarasto(unittest.TestCase):
     def test_saldo_ja_tila_tulostuu_oikein(self):
         self.varasto = Varasto(10)
         tulostus = str(self.varasto)
-        self.assertEqual(tulostus, "saldo = 0, vielä tilaa 10")
+        self.assertEqual(tulostus, "saldo = 1, vielä tilaa 10")
